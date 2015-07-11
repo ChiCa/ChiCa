586f57c56743b6a6de98fee40b3bdafc8a92df42
ammeurer <ammeurer8@gmail.com>
2015-07-11 16:52:00 -0700

    Updated git

--- chica_server.py
+++ chica_server.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import twilio.twiml
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=['GET', 'POST'])
+# def hello_world():
+#     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+
+def hello_monkey():
+    """Respond to incoming requests."""
+    resp = twilio.twiml.Response()
+    resp.say("Hello Monkey")
+ 
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
