3abf2449890635717a77b8830781f9bf1ca5b1a7
ammeurer <ammeurer8@gmail.com>
2015-07-12 09:56:11 -0700

    Added ability to record woman's message

--- chica_server.py
+++ chica_server.py
@@ -15,13 +15,13 @@ def hello_monkey():
 
 	resp = twilio.twiml.Response()
 	from_number = request.values.get('From', None)
-	# if from_number in numbers:
-	return redirect("/registered-user")
-	# else:
-	# 	numbers.append(from_number)
-	# 	with resp.gather(numDigits=12, action="/sign-up", method="POST") as g:
- # 			g.say("Welcome to ChiCa. ChiCa help you build a network of mothers who can exchange child care among eachother. To signup to the ChiCa community type in the number of your friend after the beep.")
- # 	return str(resp)
+	if from_number in numbers:
+		return redirect("/registered-user")
+	else:
+		numbers.append(from_number)
+		with resp.gather(numDigits=12, action="/sign-up", method="POST") as g:
+ 			g.say("Welcome to ChiCa. ChiCa help you build a network of mothers who can exchange child care among eachother. To signup to the ChiCa community type in the number of your friend after the beep.")
+ 	return str(resp)
 
 @app.route("/sign-up", methods=['GET', 'POST'])
 def handle_key():
@@ -62,8 +62,8 @@ def choose_day():
  		g.say("If you need child care tomorrow, press zero. If you need child care the day after tomorrow, press one.", voice="woman")
  	return str(resp)
 
-@app.route("/record-message", methods=['GET', 'POST'])
-def record_message():
+@app.route("/send-text-messages", methods=['GET', 'POST'])
+def send_text_m():
 	choice = request.values.get('Digits', None)
 	resp = twilio.twiml.Response()
 	if choice == "0":
@@ -79,6 +79,7 @@ def record_message():
 @app.route("/add-friend", methods=['GET', 'POST'])
 def add_friend():
 	resp = twilio.twiml.Response()
+
 	with resp.gather(numDigits=12, action="/confirm-add", method="GET") as g:
 		g.say("To add your friend to your chica circle, enter her number.", voice="woman")
 	return str(resp)
@@ -89,8 +90,31 @@ def confirm_add():
 
 	new_friend = request.values.get('Digits', None)
 	numbers.append(new_friend)
-	resp.say("Thank you for adding your friend to your chica circle! She is now part of the chica community. We will now forward you to the child care request menu, or you can hang up.", voice="woman")
-	return redirect("/registered-user")
+	print numbers
+	resp.say("Thank you for adding your friend to your chica circle! She is now part of the chica community. Call cheeka back to request child care", voice="woman")
+	# with resp.gather(numDigits=0, action="/registered-user", method="GET") as g:
+	# 	g.say("Thank you for adding your friend to your chica circle! She is now part of the chica community. We will now forward you to the child care request menu, or you can hang up.", voice="woman")
+	return str(resp)
+	#return redirect("/registered-user")
+
+@app.route("/record-message", methods=['GET', 'POST'])
+def record_message(): 
+	resp = twilio.twiml.Response()
+	resp.say("Record a message for your care giver after the tone.", voice="woman")
+	resp.record(maxLength="30", action="/handle-recording")
+	return str(resp)
+
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+	"""Play back the caller's recording."""
+
+	recording_url = request.values.get("RecordingUrl", None)
+
+	resp = twilio.twiml.Response()
+	resp.say("Chica will now play back your recording", voice="woman")
+	resp.play(recording_url)
+	resp.say("Goodbye.", voice="woman")
+	return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
